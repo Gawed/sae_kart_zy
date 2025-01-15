ac48733cf9ee36c7bc44a8cd2f5a0d86025040d2
Gawed <sdzy696@gmail.com>
2025-01-15 14:55:46 +0100

    Update test.py

--- test.py
+++ test.py
@@ -32,8 +32,6 @@ def sendBMSCommand(cmd):
         hex_byte = ("{0:02x}".format(cmd_byte))
         sent_cmds.append(hex_byte)
         bms.write(bytearray.fromhex(hex_byte))
-    print('\n--- BMS Command Sent: \n')
-    print(" ".join(sent_cmds))
     return
 
 # This could be much better, but it works.
@@ -62,21 +60,49 @@ def byteArrayToHEX(byte_array):
 #MAIN() - Query BMS for register 0x83 (pack total voltage)
 
 # See https://diysolarforum.com/resources/jk-bms-documentation-on-protocols-provided-by-hankzor.259/ for protocol specification documents   
-cmd = bytearray.fromhex('4E 57 00 13 00 00 00 00 03 03 00 83 00 00 00 00 68')
-crc = sum(cmd) #crc is misleading, they really just use the sum of the data so far. 
-cmd += bytearray.fromhex(f'{crc:08x}') #Crazy syntax but this formats the crc decimal value to an 8 character, zero-padded hexadecimal number and then appends it to the original cmd
+def main():
+    cmd = bytearray.fromhex('4E 57 00 13 00 00 00 00 03 03 00 83 00 00 00 00 68')
+    crc = sum(cmd) #crc is misleading, they really just use the sum of the data so far. 
+    cmd += bytearray.fromhex(f'{crc:08x}') #Crazy syntax but this formats the crc decimal value to an 8 character, zero-padded hexadecimal number and then appends it to the original cmd
+    output = readBMS(cmd)
+    #First find the length bytes
+    rawdata = bytearray.fromhex(output)
+    length = int.from_bytes(rawdata[2:4], 'big')
+    #20 Bytes would be a 1 byte data response. Increase output for bigger 
+    result = rawdata[12:(13 + (length-20))] #The returned data starts at pos 12 (byte 13) and varies in size depending on register
+    message = str(int.form_bytes(result,'big'))
+    client.publish(topic,message)
+
+broker = "192.168.1.88"
+port = 1883
+topic = "bms/baterie"
+
+client = mqtt.Client()
+client.username_pw_set("mqtt_mat","user")
+client.connect(broker,port,60)
+
+if __name__ == "__main__" : 
+    try : 
+            while True :
+                    main()
+                    sleep(10)
+    except Exception as e : 
+            print(e)
 
 
-output = readBMS(cmd)
-print('\n--- BMS Raw Response: \n')
-print(output)
-print('\n--- BMS Value: \n')
-#First find the length bytes
-rawdata = bytearray.fromhex(output)
-length = int.from_bytes(rawdata[2:4], 'big')
-#20 Bytes would be a 1 byte data response. Increase output for bigger 
 
-result = rawdata[12:(13 + (length-20))] #The returned data starts at pos 12 (byte 13) and varies in size depending on register
-print(byteArrayToHEX(result) + ' (hex) - ' + str(int.from_bytes(result, 'big')) + ' (dec)')
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 quit()
