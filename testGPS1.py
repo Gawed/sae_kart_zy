2c32cb92e4082891296e4c910c079b15bc6f4c1d
Gawed <sdzy696@gmail.com>
2025-02-24 14:35:27 +0100

    Update testGPS1.py

--- testGPS1.py
+++ testGPS1.py
@@ -5,23 +5,23 @@ from PIL import Image
 import serial
 
 def get_gps_coordinates():
-    # 直接读取串口数据
+    # lire data
     ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
     while True:
         line = ser.readline().decode('utf-8', errors='ignore')
         if line.startswith('$GPGGA'):
-            lat, lon = parse_gpgga(line)  # 使用新的解析函数
+            lat, lon = parse_gpgga(line)  
             if lat and lon:
                 return lat, lon
         time.sleep(0.5)
 
 def parse_gpgga(data):
-    """解析 GPGGA 语句，提取经纬度"""
+    """GPGGA"""
     parts = data.split(',')
     if len(parts) > 5:
-        lat = convert_to_degrees(parts[2])  # 纬度
+        lat = convert_to_degrees(parts[2])  # LAT
         lat_dir = parts[3]  # N/S
-        lon = convert_to_degrees(parts[4])  # 经度
+        lon = convert_to_degrees(parts[4])  # LON
         lon_dir = parts[5]  # E/W
         
         if lat and lon:
@@ -31,7 +31,7 @@ def parse_gpgga(data):
     return None, None
 
 def convert_to_degrees(raw_value):
-    """将 NMEA 坐标转换为十进制度"""
+    """ NMEA en decimal"""
     if not raw_value:
         return None
     try:
@@ -49,18 +49,22 @@ def get_google_map(lat, lon, api_key):
     return 'map.png'
 
 if __name__ == "__main__":
-    API_KEY = "AIzaSyDLvUcraTLttRBcvn728IaGCe_prAZK24Q"  # 替换为你的API密钥
-    
+    API_KEY = "AIzaSyDLvUcraTLttRBcvn728IaGCe_prAZK24Q"  # API GOOGLE MAP
+
     try:
         print("-------GPS---------")
-        latitude, longitude = get_gps_coordinates()
-        print(f"Coordonnées obtenues avec succès ! LAT: {latitude}, LON: {longitude}")
-        
-        map_file = get_google_map(latitude, longitude, API_KEY)
-        
-        # 显示地图图像
-        img = Image.open(map_file)
-        img.show()
-        
+
+        while True:
+            latitude, longitude = get_gps_coordinates()
+            print(f"Coordonnées obtenues avec succès ! LAT: {latitude}, LON: {longitude}")
+            
+            map_file = get_google_map(latitude, longitude, API_KEY)
+            print("map change")
+
+            img = Image.open(map_file)
+            img.show()
+
+            time.sleep(3)
+
     except Exception as e:
-        print(f"发生错误: {str(e)}")
+        print(f"error: {str(e)}")
